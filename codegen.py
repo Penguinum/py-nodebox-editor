75eaeeddc6b9434a189a2c510686ee6812b41816
Penguinum <penguinum@gmail.com>
2014-09-19 23:34:09 +0400

    Changed codegen logic.

--- codegen.py
+++ codegen.py
@@ -8,21 +8,39 @@ class codegen:
         self.nodename = nodename
         self.miniblocks = miniblocks
         self.scaling = 16.0
+        self.head = [
+            "-- Generated code\n\n",
+            "minetest.register_node(\"yourmode:yournode\", {\n",
+	    "\ttiles = {\n",
+	    "\t\t\"picture.png\",\n",
+	    "\t\t\"picture.png\",\n",
+	    "\t\t\"picture.png\",\n",
+	    "\t\t\"picture.png\",\n",
+	    "\t\t\"picture.png\",\n",
+	    "\t\t\"picture.png\",\n",
+	    "\t},\n",
+	    "\tdrawtype = \"nodebox\",\n",
+	    "\tparamtype = \"light\",\n",
+	    "\tnode_box = {\n",
+	    "\t\ttype=\"fixed\",\n",
+            "\t\tfixed={\n"
+                ]
+        self.tail = [
+		"\t\t}\n",
+                "\t},\n",
+                "\tgroups={oddly_breakable_by_hand=2}\n",
+                "})\n"
+                ]
+
 
     def writeToFile(self, export_as):
         s = self.scaling
         output_file = open(export_as, "w+")
-        output_file.write("-- Generated code\n\n")
-        output_file.write("minetest.register_node(\"modename:nodename\", {\n")
-        output_file.write("\tdrawtype = \"nodebox\",\n")
-        output_file.write("\tparamtype = \"light\",\n")
-        output_file.write("\tnode_box = {\n")
-        output_file.write("\t\ttype = \"fixed\",\n")
-        output_file.write("\t\tfixed = {\n")
+        for line in self.head:
+            output_file.write(line)
         for b in self.miniblocks:
-            output_file.write("\t\t\t{" + ", ".join([str(b.p1.x/s), str(b.p1.y/s), str(b.p1.z/s),
-                             str(b.p2.x/s), str(b.p2.y/s), str(b.p2.z/s)]) + "},\n")
-        output_file.write("\t\t}\n\t},")
-        output_file.write("\tgroups={oddly_breakable_by_hand=2}\n})")
-
+            output_file.write("\t\t\t{" + ", ".join([str(b.p1.x/s), str(b.p1.z/s), str(b.p1.y/s),
+                             str(b.p2.x/s), str(b.p2.z/s), str(b.p2.y/s)]) + "},\n")
+        for line in self.tail:
+            output_file.write(line)
         output_file.close()
