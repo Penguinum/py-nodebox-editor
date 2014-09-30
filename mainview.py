766a4477caa9d59bfbb95bf7d15ad6fe19f2e0c0
Penguinum <penguinum@gmail.com>
2014-09-30 14:20:35 +0400

    Test.

--- mainview.py
+++ mainview.py
@@ -1,8 +1,12 @@
 try:
     from PyQt5.QtWidgets import (QWidget, QSizePolicy)
-    from PyQt5.QtGui import (QPainter, QBrush, QColor)
+    from PyQt5.QtGui import (QPainter, QBrush, QColor, QPolygon)
+    from PyQt5.QtCore import Qt
+    from PyQt5 import QtCore
 except:
-    from PyQt4.QtGui import (QWidget, QSizePolicy, QPainter, QBrush, QColor)
+    from PyQt4.QtGui import (QWidget, QSizePolicy, QPainter, QBrush, QColor, QPolygon)
+    from PyQt4.QtCore import Qt
+    from PyQt4 import QtCore
 
 
 class MainView(QWidget):
@@ -20,6 +24,14 @@ class MainView(QWidget):
     def displayLines(self, p):
         x0 = self.width() / 2
         y0 = self.height() / 2
-        p.drawLine(x0, y0, x0 - 50, y0 + 50)
-        p.drawLine(x0, y0, x0, y0 - 75)
-        p.drawLine(x0, y0, x0 + 75, y0)
+        p.setPen(Qt.NoPen)
+        p.setBrush(QBrush(QColor(50, 50, 50)))
+        test = QPolygon([
+            QtCore.QPoint(100, 100),
+            QtCore.QPoint(100, 200),
+            QtCore.QPoint(200, 100)
+        ])
+        p.drawConvexPolygon(test)
+        #p.drawLine(x0, y0, x0 - 50, y0 + 50)
+        #p.drawLine(x0, y0, x0, y0 - 75)
+        #p.drawLine(x0, y0, x0 + 75, y0)
